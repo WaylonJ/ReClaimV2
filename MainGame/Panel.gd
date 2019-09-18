c627bea9fb74bfeaa42ef1584c09472ed502d99e
Waylon <waylonjones33@gmail.com>
2019-09-18 10:42:30 -0700

    Made a decent amount of UI changes, mainly around construction options and popups that give categories more details. Sizing issues took a while, learned that there seems to be an auto 'constant separation' even if that option is unchecked'

--- MainGame/Panel.gd
+++ MainGame/Panel.gd
@@ -6,24 +6,43 @@ extends Panel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var resourceBldg = "../BottomUI/MiddleSection/NoSelection/ConstructionOptions/TopRow/ResourceBldg"
+	var militaryBldg = "../BottomUI/MiddleSection/NoSelection/ConstructionOptions/TopRow/MilitaryBldg"
+	var utilityBldg = "../BottomUI/MiddleSection/NoSelection/ConstructionOptions/TopRow/UtilityBldg"
 	hide()
 	# Ok so this works functionally, still need to make it so that they hide
 	# after you unhover. Also need to somehow pass the type of button hovered over.
 	# Actualyl that might not be necessary if they all just get separate functions..
-	$"../BottomUI/MiddleSection/NoSelection/ConstructionOptions/TopRow".connect("mouse_entered", self, "_on_BottomUI_mouseEntered")
+	get_node(resourceBldg).connect("mouse_entered", self, "_on_ResourceBldg_mouseEntered")
+	get_node(resourceBldg).connect("mouse_exited", self, "hidePanel")
+	get_node(militaryBldg).connect("mouse_entered", self, "_on_MilitaryBldg_mouseEntered")
+	get_node(militaryBldg).connect("mouse_exited", self, "hidePanel")
+	get_node(utilityBldg).connect("mouse_entered", self, "_on_UtilityBldg_mouseEntered")
+	get_node(utilityBldg).connect("mouse_exited", self, "hidePanel")
 	
-func _on_BottomUI_mouseEntered():
+func _on_ResourceBldg_mouseEntered():
 	show()
 	showPanel("Resource")
+	
+func _on_MilitaryBldg_mouseEntered():
+	show()
+	showPanel("Military")
+	
+func _on_UtilityBldg_mouseEntered():
+	show()
+	showPanel("Utility")
+	
+func hidePanel():
+	hide()
 	
 func showPanel(type):
 	match type:
 		"Resource":
-			pass
+			get_node("PanelText").text = "Structures who's main focus is gathering basic resources or enhancing them into something more advanced. \n\n -Mana Pool\n -tempResourceStructre\n -Locked\n -Locked"
 		"Military":
-			pass
+			get_node("PanelText").text = "Structures that produce Units or other benefits towards your military strength.\n\n -Goblin Hut\n -Fairy Garden\n -Locked\n -Locked\n -Locked"
 		"Utility":
-			pass
+			get_node("PanelText").text = "Structures that provide a variety of unique benefits, usually involved in gathering information or enhancing some system.\n\n -Lookout Tower\n -Mana Analyzer\n -Locked"
 		_:
 			print("Everything else??")
 
